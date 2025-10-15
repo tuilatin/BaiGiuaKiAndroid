74c1dafb2d0aa6847c6a8a582f045d286e3fb157
tuilatin <167632727+tuilatin@users.noreply.github.com>
2025-10-15 08:18:17 +0700

    Delete Bai4 directory

--- Bai4/src/main/java/org/example/KhoiC.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class KhoiC extends KhoiA {
-	public KhoiC() {}
-
-	public KhoiC(String mon1_144, String mon2_144, String mon3_144) {
-		super(mon1_144, mon2_144, mon3_144);
-	}
-}
